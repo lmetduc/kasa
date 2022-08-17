8aa526d406dbbe6d8278f5bb6109d4d5fc3ffaec
lmetduc <93624119+lmetduc@users.noreply.github.com>
2022-08-17 19:33:14 +0200

    carrousel one picture

--- src/components/Carousel/Carousel.jsx
+++ src/components/Carousel/Carousel.jsx
@@ -1,46 +1,66 @@
-import React, {useState} from 'react';
-import './Carousel.css';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faChevronLeft, faChevronRight } from '@fortawesome/free-solid-svg-icons'
+import React, { useState } from "react";
+import "./Carousel.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faChevronLeft,
+  faChevronRight,
+} from "@fortawesome/free-solid-svg-icons";
 
 export default function Carousel(props) {
-
-    const [slideIndex, setSlideIndex] = useState(0);
-    const imgSize = () => {
-        const carouselshowImg = document.querySelector('.carousel-container img');
-        if(!carouselshowImg){
-            return 0;
-        }
-        return carouselshowImg.width;
+  const [slideIndex, setSlideIndex] = useState(0);
+  const imgSize = () => {
+    const carouselshowImg = document.querySelector(".carousel-container img");
+    if (!carouselshowImg) {
+      return 0;
     }
+    return carouselshowImg.width;
+  };
 
-    const onNext = () => {
-        if(slideIndex === props.img.length - 1){
-            setSlideIndex(0)
-        } else {
-            setSlideIndex(slideIndex + 1)
-        }
+  const onNext = () => {
+    if (slideIndex === props.img.length - 1) {
+      setSlideIndex(0);
+    } else {
+      setSlideIndex(slideIndex + 1);
     }
+  };
 
-    const onPrev = () => {
-        if(slideIndex === 0){
-            setSlideIndex(props.img.length - 1)
-        } else {
-            setSlideIndex(slideIndex - 1)
-        }
+  const onPrev = () => {
+    if (slideIndex === 0) {
+      setSlideIndex(props.img.length - 1);
+    } else {
+      setSlideIndex(slideIndex - 1);
     }
+  };
 
-    return (
-        <div className="carousel">
-            <div className="carousel-container" style={{transform: `translateX(-${slideIndex * imgSize()}px)`}}>
-            {props.img.map((pictures) => <img className='carousel-img' src={pictures} key={pictures}/>)}
-            </div>
-            <div className='carousel-controls'>
-            <FontAwesomeIcon icon={faChevronLeft} className="chevron" onClick={onPrev}/>
-            <FontAwesomeIcon icon={faChevronRight} className="chevron" onClick={onNext}/>
-
-            </div>
-            <div className='carousel-index'>{slideIndex + 1} / {props.img.length}</div>
+  return (
+    <div className="carousel">
+      <div
+        className="carousel-container"
+        style={{ transform: `translateX(-${slideIndex * imgSize()}px)` }}
+      >
+        {props.img.map((pictures) => (
+          <img className="carousel-img" src={pictures} key={pictures} />
+        ))}
+      </div>
+      {props.img.length > 1 && (
+        <div className="carousel-controls">
+          <FontAwesomeIcon
+            icon={faChevronLeft}
+            className="chevron"
+            onClick={onPrev}
+          />
+          <FontAwesomeIcon
+            icon={faChevronRight}
+            className="chevron"
+            onClick={onNext}
+          />
         </div>
-    )
+      )}
+      {props.img.length > 1 && (
+        <div className="carousel-index">
+          {slideIndex + 1} / {props.img.length}
+        </div>
+      )}
+    </div>
+  );
 }
